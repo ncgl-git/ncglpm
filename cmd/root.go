e85df015676cc67902c4960b8035b0901b9a4e73
Nicholas Lawrence <ncg-l@outlook.com>
2023-08-05 11:58:40 -0500

    version

--- cmd/root.go
+++ cmd/root.go
@@ -10,6 +10,7 @@ import (
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "ncglpm",
+	Version: "v0.0.1b",
 	Short: "A brief description of your application",
 	Long: `A longer description that spans multiple lines and likely contains
 examples and usage of using your application..`,
